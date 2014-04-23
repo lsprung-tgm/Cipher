1d04c8b02237f611ea2d02a99f0c0fa04883a261
Daniel Melichar <danielmelichar0905@gmail.com>
2014-04-23 10:06:05 +0200

    JUnit Test cases

---
+++ Decypher/src/melicharsprung/test/KeywordCipher_test.java
@@ -0,0 +1,19 @@
+package melicharsprung.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Daniel Melichar
+ *
+ */
+public class KeywordCipher_test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Decypher/src/melicharsprung/test/ShiftCipher_test.java
@@ -0,0 +1,19 @@
+package melicharsprung.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Daniel Melichar
+ *
+ */
+public class ShiftCipher_test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

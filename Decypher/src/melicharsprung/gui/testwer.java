7d552a4847ac79bdb6ba40198ae0b128d6b63557
Daniel Melichar <danielmelichar0905@gmail.com>
2014-05-12 11:42:29 +0200

    Final changes

--- Decypher/src/melicharsprung/gui/testwer.java
+++
@@ -1,7 +0,0 @@
-package melicharsprung.gui;
-
-public class testwer {
-	public static void main(String[] args) {
-		InterfaceCipherControl icc = new InterfaceCipherControl();
-	}
-}
---
+++ Decypher/src/melicharsprung/start/main.java
@@ -0,0 +1,8 @@
+package melicharsprung.start;
+import melicharsprung.gui.*;
+
+public class main {
+	public static void main(String[] args) {
+		new InterfaceCipher();
+	}
+}
